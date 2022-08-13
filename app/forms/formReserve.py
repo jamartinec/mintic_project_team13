297064e308cd88cf2007db6caf063d2faecf6b5a
Camilo201Diaz <camilo201diaz@gmail.com>
2022-08-13 15:34:55 -0500

    registrar reserva fallido

--- app/forms/formReserve.py
+++ app/forms/formReserve.py
@@ -1,12 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField
-from wtforms.validators import DataRequired, Length, EqualTo 
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField, IntegerField, DateField
+from wtforms.validators import DataRequired, Length, EqualTo, NumberRange
 
 # Clase del formulario, se agrega las validaciones
 
 
 class FormReserve(FlaskForm):
-    dateIni = StringField("Fecha de ingreso", validators=[DataRequired(message='Campo nombre requerido')])
-    dateFin = StringField("Fecha de salida", validators=[DataRequired(message='Campo usuario requerido')])
-    cantPerson = StringField("Cantidad de personas", validators=[DataRequired(message='Campo Contraseña requerido')])
+    dateIni = DateField("Fecha de ingreso", validators=[DataRequired(message='Campo fecha de inicio requerido')])
+    dateFin = DateField("Fecha de salida", validators=[DataRequired(message='Campo fecha de salida requerido')])
+    cantPerson = IntegerField("Cantidad de personas", validators=[DataRequired(message='mínimo 1 - maximo 10 huéspedes'), NumberRange(min=1, max=10)])
     reserveForm = SubmitField("Reservar habitación")
+
+    
